bc213b08d20f7fc2a70bdcc58c0ecb6d4765125c
ksankakovsh <ksanka.kovsh@gmail.com>
2023-06-01 20:52:14 +0300

    feat: add secondsection and footer without adaptiv

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -19,4 +19,41 @@ export function isWebp(){
         });
 
 }
+// custom select
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+const optionsList = document.querySelectorAll(".option");
 
+selected.addEventListener("click", () => {
+    selected.classList.toggle("selected-hover")
+    optionsContainer.classList.toggle("active");
+    });
+
+    optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+    });
+});
+// input range
+function rangeValue() {
+	const rangeInput = document.getElementById('percent'); 
+	let value = document.getElementById('value'); 
+	value.innerHTML = rangeInput.value;
+}
+// input file
+let inputs = document.querySelectorAll('.input_file');
+        Array.prototype.forEach.call(inputs, function (input) {
+        let label = input.nextElementSibling,
+            labelVal = label.querySelector('.input_file-text').innerText;
+        input.addEventListener('change', function (e) {
+            let countFiles = '';
+            if (this.files && this.files.length >= 1)
+            countFiles = this.files.length;
+    
+            if (countFiles)
+            label.querySelector('.input_file-text').innerText = 'Выбрано файлов: ' + countFiles;
+            else
+            label.querySelector('.input_file-text').innerText = labelVal;
+        });
+        });
